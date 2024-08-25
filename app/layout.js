053a2fad75aea09af2d1c6e557a65fb4599d0509
syedfaheem27 <syedfaheem2719@gmail.com>
2024-08-26 00:44:47 +0530

    Add Home page styles

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -22,14 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} relative bg-primary-950 text-primary-100 min-h-screen`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright &#169; The wild oasis</footer>
+        <Header />
+        <div className=" min-h-screen px-8 py-12">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
