ce2c4ffbd3a92f3c4178093ed56e5b19af46a7de
syedfaheem27 <syedfaheem2719@gmail.com>
2024-08-26 12:35:17 +0530

    Add the account page layout, styles and nested pages

--- app/layout.js
+++ app/layout.js
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} relative bg-primary-950 text-primary-100 min-h-screen`}
       >
         <Header />
-        <div className=" min-h-screen px-8 py-12">
-          <main className="max-w-7xl  mx-auto">{children}</main>
+        <div className=" min-h-screen px-8 py-12 grid">
+          <main className="max-w-7xl  mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
