be79d655613ba5b102ea76d945e0c5a1e2d38642
syedfaheem27 <syedfaheem2719@gmail.com>
2024-09-05 13:56:28 +0530

    Revert "Protect account page"
    This reverts commit 08e86adef4229eac43626d2d18c6ccd6044fab86.

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -4,6 +4,7 @@ import { authConfig } from "../_lib/auth";
 
 export default async function Navigation() {
   const session = await getServerSession(authConfig);
+  console.log(session);
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
--- app/account/page.js
+++ app/account/page.js
@@ -1,15 +1,11 @@
-import { getServerSession } from "next-auth";
-import { authConfig } from "../_lib/auth";
-
 export const metadata = {
   title: "Guest Area",
 };
 
-export default async function Page() {
-  const session = await getServerSession(authConfig);
+export default function Page() {
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome {session?.user?.name ?? "User"}
+      Welcome Faheem
     </h2>
   );
 }
