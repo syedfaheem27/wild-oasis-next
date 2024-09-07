929d79f3dd058a55b28918bdf1ba95f617a126fb
syedfaheem27 <syedfaheem2719@gmail.com>
2024-09-07 16:09:54 +0530

    Add a loading indicator while updating guest profile

--- app/_components/UploadProfileForm.js
+++ app/_components/UploadProfileForm.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { updateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
 export default function UploadProfileForm({ children, guest }) {
   const { fullName, nationalID, countryFlag, email } = guest;
@@ -53,10 +54,21 @@ export default function UploadProfileForm({ children, guest }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      disabled={pending}
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
